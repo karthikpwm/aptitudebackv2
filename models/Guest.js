7f1af9e8576c654307557ecf5515ab1c677b034a
karthikpwm <karthik@pwm-india.com>
2022-07-26 13:33:07 +0530

    jbjkb

--- models/Guest.js
+++ models/Guest.js
@@ -5,11 +5,11 @@ exports.insertcandidate = async (param) => {
   try {
     await con.beginTransaction();
     const result = await con.query("INSERT INTO candidatedetails (name,position, email, mobile,company_id,ctc,pincode,category_id) VALUE ( ?, ?, ?, ?, ?,?,?,? ) ",
-      [param.name, param.position, param.email, param.mobile, param.company_id, param.ctc, param.pincode,param.category_id])
+      [param.name, param.position, param.email, param.mobile, param.company_id, param.ctc, param.pincode, param.category_id])
     await con.commit();
     await con.beginTransaction();
-    const test = await con.query("INSERT INTO candidatetestlog (candidate_id,test, createdate,company_id,timelimit,category_id) VALUE ( ?, ?, NOW(), ?, ?, ?) ",
-      [result[0].insertId, 0, param.company_id, param.timelimit,param.category_id])
+    const test = await con.query("INSERT INTO candidatetestlog (candidate_id,test, createdate,company_id,timelimit,category_id,selection) VALUE ( ?, ?, NOW(), ?, ?, ?, ?) ",
+      [result[0].insertId, 0, param.company_id, param.timelimit, param.category_id, 0])
     await con.commit();
     return result[0].insertId;
   } catch (err) {
